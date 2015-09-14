5aed84d1f156fa4aca3b7e7adb318ea2f0d1dc71
97marcar <hans.hansson22@gmail.com>
2015-09-14 21:05:16 +0200

    Uppgifter 15-14-09
    kom igen...

---
+++ Uppgifter 15-14-09.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 20:56:37 2015
+
+@author: 97marcar
+"""
+#1
+name = input("Namn: ")
+print("Hejsan", name + "!")
+
+#2
+tal1 = int(input("Tal 1: "))
+tal2 = int(input("Tal 2: "))
+print("Summa av talen =", tal1+tal2)
