450c7cd38e4dfce7934664a1262e95d8c2232869
97marcar <hans.hansson22@gmail.com>
2015-11-03 09:38:26 +0100

    basics for model
    Basics for model done according to instructions.

---
+++ exam1/model.py
@@ -0,0 +1,8 @@
+
+class Model:
+
+    def __init__(self):
+        """The constructor of the model"""
+        self.lista_BR = [] #the list that will contain the Blueray movies
+        self.lista_DVD = [] #the list that will contain the regular DVD movies
+        self.lista_VHS = [] #the list that will contain the VHS movies
