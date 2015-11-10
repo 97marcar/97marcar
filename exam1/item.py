a63bbbd01f46fcd29bdcfe6d1d216ec8312ac2cf
97marcar <hans.hansson22@gmail.com>
2015-11-10 09:42:18 +0100

    Started with the item class
    started with the item class, gave it attributes

---
+++ exam1/item.py
@@ -0,0 +1,12 @@
+class item:
+    
+    def __init_(self, movie_type, name, genre, director, year):
+        """..."""
+        self._movie_type = movie_type
+        self._name = name
+        self._genre = genre
+        self._director = director
+        self._year = year
+        
+    
+        
