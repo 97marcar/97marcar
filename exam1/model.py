edd2f3f0a9cd0cab592d3824d18fb6f6c504b644
97marcar <hans.hansson22@gmail.com>
2015-11-11 17:44:11 +0100

    Tested so the model can add the movie
    made sure the model can add the movie to the correct list, works!

--- exam1/model.py
+++ exam1/model.py
@@ -1,4 +1,4 @@
-
+from item import *
 class Model:
 
     def __init__(self):
@@ -6,3 +6,18 @@ class Model:
         self.lista_BR = [] #the list that will contain the Blueray movies
         self.lista_DVD = [] #the list that will contain the regular DVD movies
         self.lista_VHS = [] #the list that will contain the VHS movies
+        
+    def add_movie(self, movie):
+        self.lista_DVD.append(movie)
+            
+            
+            
+def main():
+    new_model = Model()
+    dvd = DVD("123")
+    new_model.add_movie(dvd)
+    print(dvd.info())
+    print(new_model.lista_DVD)
+    
+if __name__ == "__main__":
+    main()
