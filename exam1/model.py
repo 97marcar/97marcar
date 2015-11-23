0dcb02033039fdd8310f09897c59749146858f5b
97marcar <hans.hansson22@gmail.com>
2015-11-23 18:15:57 +0100

    you can now create and item from __init__

--- exam1/model.py
+++ exam1/model.py
@@ -7,13 +7,22 @@ class Model:
         self.lista_BR = [] #the list that will contain the Blueray movies
         self.lista_DVD = [] #the list that will contain the regular DVD movies
         self.lista_VHS = [] #the list that will contain the VHS movies
-        self.create_item()
         
     def add_movie(self, movie):
         self.lista_DVD.append(movie)
             
             
             
-    def create_item(self):
+    def create_item(self, movie_type, unique, name, genre, director, year):
+        if movie_type == "DVD":
+            self.dvd = DVD(unique, name, genre, director, year)
+            print(self.dvd.info())
+        if movie_type == "Blueray":
+            self.br = BR(unique, name, genre, director, year)
+            print(self.br.info())
+        if movie_type == "VHS":
+            self.vhs = VHS(unique, name, genre, director, year)
+            print(self.vhs.info())
+        
         self.item = DVD("3h", "Interstellar", "SCI-FI", "Christoper Nolan", "2014")
         self.add_movie(self.item)
