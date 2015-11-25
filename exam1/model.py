e3d76e70b8b25b9800a7302b72f73d5456198b1b
97marcar <hans.hansson22@gmail.com>
2015-11-25 16:26:24 +0100

    Added set and get save
    Made get and set saves for all the lists, you don't loose your register
    when shutting down the program.

--- exam1/model.py
+++ exam1/model.py
@@ -7,11 +7,8 @@ class Model:
         self.lista_BR = [] #the list that will contain the Blueray movies
         self.lista_DVD = [] #the list that will contain the regular DVD movies
         self.lista_VHS = [] #the list that will contain the VHS movies
+
         
-    def add_movie(self, movie):
-        self.lista_DVD.append(movie)
-            
-            
             
     def create_item(self, movie_type, unique, name, genre, director, year):
         if movie_type == "DVD":
@@ -27,6 +24,81 @@ class Model:
             self.lista_VHS.append(self.vhs)
             print(self.vhs.info())
             
-    def get_item(self):
+    def get_itemsBR(self):
         return self.lista_BR
+        
+        
+    def get_itemsDVD(self):
+        return self.lista_DVD
+        
+    def get_itemsVHS(self):
+        return self.lista_VHS
+        
+    def set_saveBR(self):
+        try:
+            file = open("./saveBR.csv", "w")
+        except:
+            print("Wrong when saving to file.")
+        else:
+            for item in self.lista_BR:
+                file.write(item.__str__())
             
+            file.close() 
+            
+    def get_saveBR(self):
+        try:        
+            file = open("./saveBR.csv", "r")
+        except:
+            print("Wrong when opening file.")
+        else:
+            for line in file:
+                line.strip()
+                item = line.split(";")
+                self.lista_BR.append(BR(item[4], item[1], item[2], item[3], item[0]))
+            file.close()
+            
+    def set_saveDVD(self):
+        try:
+            file = open("./saveDVD.csv", "w")
+        except:
+            print("Wrong when saving to file.")
+        else:
+            for item in self.lista_DVD:
+                file.write(item.__str__())
+            
+            file.close() 
+            
+    def get_saveDVD(self):
+        try:        
+            file = open("./saveDVD.csv", "r")
+        except:
+            print("Wrong when opening file.")
+        else:
+            for line in file:
+                line.strip()
+                item = line.split(";")
+                self.lista_DVD.append(DVD(item[4], item[1], item[2], item[3], item[0]))
+            file.close()
+            
+    def set_saveVHS(self):
+        try:
+            file = open("./saveVHS.csv", "w")
+        except:
+            print("Wrong when saving to file.")
+        else:
+            for item in self.lista_VHS:
+                file.write(item.__str__())
+            
+            file.close() 
+            
+    def get_saveVHS(self):
+        try:        
+            file = open("./saveVHS.csv", "r")
+        except:
+            print("Wrong when opening file.")
+        else:
+            for line in file:
+                line.strip()
+                item = line.split(";")
+                self.lista_VHS.append(VHS(item[4], item[1], item[2], item[3], item[0]))
+            file.close()
