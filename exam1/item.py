949340aeaf705df1db236725bbbe94e544652c25
97marcar <hans.hansson22@gmail.com>
2015-11-26 10:14:42 +0100

    Added some docstrings in the item file

--- exam1/item.py
+++ exam1/item.py
@@ -1,8 +1,8 @@
 
 class Item:
-    
+    """Creates a base of an item class"""
     def __init__(self, name, genre, director, year):
-        """..."""
+        """The constructor of the Item class"""
         self.movie_type = "movie"
         self.name = name
         self.genre = genre
@@ -14,9 +14,9 @@ class Item:
         return({"type": self.movie_type, "Name": self.name, "Genre": self.genre, "Director": self.director, "Year": self.year})
         
 class DVD(Item):
-    """..."""
+    """Uses the Item class to make a DVD to add to the register"""
     def __init__(self, length, name, genre, director, year):
-        """..."""
+        """The constructor of the DVD class"""
         super().__init__(name, genre, director, year)
         self.movie_type = "DVD"
         self.length = length
@@ -31,9 +31,9 @@ class DVD(Item):
         return(self.name+";"+self.genre+";"+self.director+";"+self.year+";"+self.length+";"+"\n")
         
 class BR(Item):
-    """..."""
+    """Uses the Item class to make a Blueray to add to the register"""
     def __init__(self, resolution, name, genre, director, year):
-        """..."""
+        """The constructor of the BR class"""
         super().__init__(name, genre, director, year)
         self.movie_type = "Blueray"
         self.resolution = resolution
@@ -48,9 +48,9 @@ class BR(Item):
         return(self.name+";"+self.genre+";"+self.director+";"+self.year+";"+self.resolution+";"+"\n")
 
 class VHS(Item):
-    """..."""
+    """Uses the Item class to make a VHS to add to the register"""
     def __init__(self, color, name, genre, director, year):
-        """..."""
+        """The constructor for the VHS class"""
         super().__init__(name, genre, director, year)
         self.movie_type = "VHS"
         self.color = color
